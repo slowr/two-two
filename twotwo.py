a2d1b2ef4ac827bb26f7602b150167dd55d6daf8
slowr <blackeye_l2@yahoo.gr>
2017-01-11 20:33:20 +0200

    Added some additional explanation to the code

--- twotwo.py
+++ twotwo.py
@@ -4,6 +4,7 @@ def main():
 	tmp = 1
 	root = {}
 	add_keyword(root,str(tmp))
+	# Set highest power of 2 in dictionary to 2^800
 	for i in range(800):
 		tmp *= 2
 		add_keyword(root,str(tmp))
@@ -16,6 +17,11 @@ def main():
 	print('\n'.join(result))
 
 def add_keyword(root, keyword):
+	'''
+		We add to the dictionary (aho-corasick tree) the keyword. At the last letter of the
+		keyword we set a new value 'p' in the dictionary for us to know that at this depth
+		of the tree this is a power of 2.
+	'''
 	length = len(keyword)
 	node = root
 	# Iterate through all characters
@@ -33,6 +39,11 @@ def add_keyword(root, keyword):
 			node = node[char]
 
 def find_keyword(root, keyword):
+	'''
+		We follow the letters of the string and for each occurence of the value 'p' we increment
+		the counter of the powers of two that we find. After we repeat this procedure for each
+		substring moving the starting index only.
+	'''
 	counter = 0
 	length = len(keyword)
 	for i in range(length):
@@ -42,6 +53,7 @@ def find_keyword(root, keyword):
 			if char in node:
 				node = node[char]
 				if('p' in node):
+					# The below three computations are different in performance. Need to check for best.  
 					# counter = -~counter
 					counter = counter + 1
 					# counter += 1
